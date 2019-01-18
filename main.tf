d7610e1c4c103c16fa7d85d815bd5ee30fea2c1d
Brad Soper <brad.soper@gmail.com>
2019-01-18 13:28:03 -0800

    Added support for the 3rd SVM

--- main.tf
+++ main.tf
@@ -45,6 +45,64 @@ module "SDSvm1_Disk4"{
   disk_size               = "${var.sdsvm1_disk4_disk_size}"
 }
 
+module "SDSvm2_Disk1"{
+  source                  = "Services/CreateVMDK"
+  disk_datastore          = "${var.sdsvm2_disk1_datastore}"
+  disk_attach_path_name   = "${var.sdsvm2_disk1_attach_path_name}"
+  disk_size               = "${var.sdsvm2_disk1_disk_size}"
+}
+
+module "SDSvm2_Disk2"{
+  source                  = "Services/CreateVMDK"
+  disk_datastore          = "${var.sdsvm2_disk2_datastore}"
+  disk_attach_path_name   = "${var.sdsvm2_disk2_attach_path_name}"
+  disk_size               = "${var.sdsvm2_disk2_disk_size}"
+}
+
+module "SDSvm2_Disk3"{
+  source                  = "Services/CreateVMDK"
+  disk_datastore          = "${var.sdsvm2_disk3_datastore}"
+  disk_attach_path_name   = "${var.sdsvm2_disk3_attach_path_name}"
+  disk_size               = "${var.sdsvm2_disk3_disk_size}"
+}
+
+module "SDSvm2_Disk4"{
+  source                  = "Services/CreateVMDK"
+  disk_datastore          = "${var.sdsvm2_disk4_datastore}"
+  disk_attach_path_name   = "${var.sdsvm2_disk4_attach_path_name}"
+  disk_size               = "${var.sdsvm2_disk4_disk_size}"
+}
+
+module "SDSvm3_Disk1"{
+  source                  = "Services/CreateVMDK"
+  disk_datastore          = "${var.sdsvm3_disk1_datastore}"
+  disk_attach_path_name   = "${var.sdsvm3_disk1_attach_path_name}"
+  disk_size               = "${var.sdsvm3_disk1_disk_size}"
+}
+
+module "SDSvm3_Disk2"{
+  source                  = "Services/CreateVMDK"
+  disk_datastore          = "${var.sdsvm3_disk2_datastore}"
+  disk_attach_path_name   = "${var.sdsvm3_disk2_attach_path_name}"
+  disk_size               = "${var.sdsvm3_disk2_disk_size}"
+}
+
+module "SDSvm3_Disk3"{
+  source                  = "Services/CreateVMDK"
+  disk_datastore          = "${var.sdsvm3_disk3_datastore}"
+  disk_attach_path_name   = "${var.sdsvm3_disk3_attach_path_name}"
+  disk_size               = "${var.sdsvm3_disk3_disk_size}"
+}
+
+module "SDSvm3_Disk4"{
+  source                  = "Services/CreateVMDK"
+  disk_datastore          = "${var.sdsvm3_disk4_datastore}"
+  disk_attach_path_name   = "${var.sdsvm3_disk4_attach_path_name}"
+  disk_size               = "${var.sdsvm3_disk4_disk_size}"
+}
+
+
+
 module "SDSvm1"{
   source             = "Services/SDS"
   root_password      = "${var.root_password}"
@@ -62,4 +120,42 @@ module "SDSvm1"{
   disk3_datastore    = "${module.SDSvm1_Disk3.disk1_datastore_id}"
   disk4_attach_path  = "${var.sdsvm1_disk4_attach_path_name}"
   disk4_datastore    = "${module.SDSvm1_Disk4.disk1_datastore_id}"
+}
+
+module "SDSvm2"{
+  source             = "Services/SDS"
+  root_password      = "${var.root_password}"
+  servers            = "${var.sdsvm2_servers}"
+  datastore          = "${var.sdsvm2_datastore}"
+  ipv4_344           = "${var.sdsvm2_ipv4_344}"
+  ipv4_siopg1        = "${var.sdsvm2_ipv4_siopg1}"
+  ipv4_siopg2        = "${var.sdsvm2_ipv4_siopg2}"
+  server_name        = "${var.sdsvm2_server_name}"
+  disk1_attach_path  = "${var.sdsvm2_disk1_attach_path_name}"
+  disk1_datastore    = "${module.SDSvm2_Disk1.disk1_datastore_id}"
+  disk2_attach_path  = "${var.sdsvm2_disk2_attach_path_name}"
+  disk2_datastore    = "${module.SDSvm2_Disk2.disk1_datastore_id}"
+  disk3_attach_path  = "${var.sdsvm2_disk3_attach_path_name}"
+  disk3_datastore    = "${module.SDSvm2_Disk3.disk1_datastore_id}"
+  disk4_attach_path  = "${var.sdsvm2_disk4_attach_path_name}"
+  disk4_datastore    = "${module.SDSvm2_Disk4.disk1_datastore_id}"
+}
+
+module "SDSvm3"{
+  source             = "Services/SDS"
+  root_password      = "${var.root_password}"
+  servers            = "${var.sdsvm3_servers}"
+  datastore          = "${var.sdsvm3_datastore}"
+  ipv4_344           = "${var.sdsvm3_ipv4_344}"
+  ipv4_siopg1        = "${var.sdsvm3_ipv4_siopg1}"
+  ipv4_siopg2        = "${var.sdsvm3_ipv4_siopg2}"
+  server_name        = "${var.sdsvm3_server_name}"
+  disk1_attach_path  = "${var.sdsvm3_disk1_attach_path_name}"
+  disk1_datastore    = "${module.SDSvm3_Disk1.disk1_datastore_id}"
+  disk2_attach_path  = "${var.sdsvm3_disk2_attach_path_name}"
+  disk2_datastore    = "${module.SDSvm3_Disk2.disk1_datastore_id}"
+  disk3_attach_path  = "${var.sdsvm3_disk3_attach_path_name}"
+  disk3_datastore    = "${module.SDSvm3_Disk3.disk1_datastore_id}"
+  disk4_attach_path  = "${var.sdsvm3_disk4_attach_path_name}"
+  disk4_datastore    = "${module.SDSvm3_Disk4.disk1_datastore_id}"
 }
--- variables.tf
+++ variables.tf
@@ -42,6 +42,45 @@ variable "sdsvm1_disk4_datastore"{}
 variable "sdsvm1_disk4_attach_path_name"{}
 variable "sdsvm1_disk4_disk_size"{}
 
+# SDSvm2 Disk 1 variables
+variable "sdsvm2_disk1_datastore"{}
+variable "sdsvm2_disk1_disk_size"{}
+variable "sdsvm2_disk1_attach_path_name"{}
+
+# SDSvm2 Disk 2 variables
+variable "sdsvm2_disk2_datastore"{}
+variable "sdsvm2_disk2_disk_size"{}
+variable "sdsvm2_disk2_attach_path_name"{}
+
+# SDSvm2 Disk 3 variables
+variable "sdsvm2_disk3_datastore"{}
+variable "sdsvm2_disk3_disk_size"{}
+variable "sdsvm2_disk3_attach_path_name"{}
+
+# SDSvm2 Disk 4 variables
+variable "sdsvm2_disk4_datastore"{}
+variable "sdsvm2_disk4_disk_size"{}
+variable "sdsvm2_disk4_attach_path_name"{}
+
+# SDSvm3 Disk 1 variables
+variable "sdsvm3_disk1_datastore"{}
+variable "sdsvm3_disk1_disk_size"{}
+variable "sdsvm3_disk1_attach_path_name"{}
+
+# SDSvm3 Disk 2 variables
+variable "sdsvm3_disk2_datastore"{}
+variable "sdsvm3_disk2_disk_size"{}
+variable "sdsvm3_disk2_attach_path_name"{}
+
+# SDSvm3 Disk 3 variables
+variable "sdsvm3_disk3_datastore"{}
+variable "sdsvm3_disk3_disk_size"{}
+variable "sdsvm3_disk3_attach_path_name"{}
+
+# SDSvm3 Disk 4 variables
+variable "sdsvm3_disk4_datastore"{}
+variable "sdsvm3_disk4_disk_size"{}
+variable "sdsvm3_disk4_attach_path_name"{}
 
 # SDSvm1 Server variables
 variable "sdsvm1_servers"{}
@@ -50,3 +89,19 @@ variable "sdsvm1_ipv4_344"{}
 variable "sdsvm1_ipv4_siopg1"{}
 variable "sdsvm1_ipv4_siopg2"{}
 variable "sdsvm1_server_name"{}
+
+# SDSvm2 Server variables
+variable "sdsvm2_servers"{}
+variable "sdsvm2_datastore"{}
+variable "sdsvm2_ipv4_344"{}
+variable "sdsvm2_ipv4_siopg1"{}
+variable "sdsvm2_ipv4_siopg2"{}
+variable "sdsvm2_server_name"{}
+
+# SDSvm3 Server variables
+variable "sdsvm3_servers"{}
+variable "sdsvm3_datastore"{}
+variable "sdsvm3_ipv4_344"{}
+variable "sdsvm3_ipv4_siopg1"{}
+variable "sdsvm3_ipv4_siopg2"{}
+variable "sdsvm3_server_name"{}
